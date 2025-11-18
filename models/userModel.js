4b779c6aed636acfe67969605dd2cc7865355773
= <mukhtarrahimi.dev@gmail.com>
2025-11-18 13:23:32 +0430

    userModel complate

--- models/userModel.js
+++ models/userModel.js
@@ -1,31 +1,40 @@
 const mongoose = require('mongoose');
 
-const userSchema = mongoose.Schema({
-  userName: {
-    type: String,
-    required: [true, 'username is required'],
+const userSchema = mongoose.Schema(
+  {
+    userName: {
+      type: String,
+      required: [true, 'username is required'],
+    },
+    email: {
+      type: String,
+      required: [true, 'email is required'],
+      unique: true,
+    },
+    address: {
+      type: Array,
+    },
+    phone: {
+      type: String,
+      required: [true, 'phone is required'],
+    },
+    password: {
+      type: String,
+      required: [true, 'password is required'],
+    },
+    usertype: {
+      type: String,
+      required: [true, 'usertype is required'],
+      default: 'client',
+      enum: ['client', 'admin', 'vendor', 'driver'],
+    },
+    profile: {
+      type: String,
+      default:
+        'https://icons.veryicon.com/png/o/miscellaneous/two-color-webpage-small-icon/user-244.png',
+    },
   },
-  email: {
-    type: String,
-    required: [true, 'email is required'],
-    unique: true,
-  },
-  address: {
-    type: Array,
-  },
-  phone: {
-    type: String,
-    required: [true, 'phone is required'],
-  },
-  password: {
-    type: String,
-    required: [true, 'password is required'],
-  },
-  usertype: {
-    type: String,
-    required: [true, 'usertype is required'],
-    default: 'client',
-    enum: ['client', 'admin', 'vendor', 'driver'],
-  },
-  profile: {},
-});
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('User', userSchema);
