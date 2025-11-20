e9aa66abf0676762abe5df260fdfadd9265b9c58
= <mukhtarrahimi.dev@gmail.com>
2025-11-20 09:30:50 +0430

    userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,21 @@
-// const User = require('../models/userModel');
+const User = require('../models/userModel');
+
 const getUser = async (req, res) => {
   try {
-    res.send('User Data');
+    // find user
+    const user = await User.findById({ _id: req.body.id });
+    // validation
+    if (!user) {
+      return res.status(404).send({
+        success: false,
+        message: 'User Not Found!',
+      });
+    }
   } catch (err) {
     console.log(err);
     res.status(500).send({
       success: false,
-      message: 'server error',
+      message: 'Error In Get User API',
       err,
     });
   }
