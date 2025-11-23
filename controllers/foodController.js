fc0d87cd2033fb3f6ff3f14288ddad627c6e32f9
= <mukhtarrahimi.dev@gmail.com>
2025-11-23 15:20:59 +0430

    order

--- controllers/foodController.js
+++ controllers/foodController.js
@@ -1,4 +1,5 @@
 const Food = require('../models/foodModel');
+const Order = require('../models/orderModel');
 
 // CREATE FOOD
 const createFood = async (req, res) => {
@@ -233,4 +234,41 @@ const placeOrder = async (req, res) => {
   }
 };
 
-module.exports = { createFood, getAllFood, updateFood, deleteFood, placeOrder };
+// CHANGE ORDER STATUS
+const orderStatusController = async (req, res) => {
+  try {
+    const orderId = req.params.id;
+    if (!orderId) {
+      return res.status(404).send({
+        success: false,
+        message: 'Please Provide valid order id',
+      });
+    }
+    const { status } = req.body;
+    const order = await orderModel.findByIdAndUpdate(
+      orderId,
+      { status },
+      { new: true }
+    );
+    res.status(200).send({
+      success: true,
+      message: 'Order Status Updated',
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: 'Error In Order Status API',
+      error,
+    });
+  }
+};
+
+module.exports = {
+  createFood,
+  getAllFood,
+  updateFood,
+  deleteFood,
+  placeOrder,
+  orderStatusController,
+};
--- routes/foodRoutes.js
+++ routes/foodRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middleware/authMiddleware');
+const adminMiddleware = require('../middleware/adminMiddleware');
 const foodController = require('../controllers/foodController');
 // CREATE || POST
 router.post('/create', authMiddleware, foodController.createFood);
@@ -9,5 +10,12 @@ router.put('/update/:id', authMiddleware, foodController.updateFood);
 router.delete('/delete/:id', authMiddleware, foodController.deleteFood);
 // ORDER
 router.post('/placeorder', authMiddleware, foodController.placeOrder);
+// ORDER STATUS
+router.post(
+  '/orderStatus/:id',
+  authMiddleware,
+  adminMiddleware,
+  foodController.orderStatusController
+);
 
 module.exports = router;
